5848ffc63e5ab2951dc720286fb74f5ac97d09ab
Manchdog <116804617+Manchdog@users.noreply.github.com>
2022-12-07 18:17:09 +0300

    Add files via upload

---
+++ lab67.h
@@ -0,0 +1,8 @@
+#ifndef LAB67
+#define LAB67
+int ost(std::string result, std::string N, int num);
+std::string to_str(int x);
+int to_int(char str);
+int randomnum(int min, int max);
+int correct_data(std::string un, char s, int y, int m, int d);
+#endif
