ced4ec7bd8c6c3942807679e4205d4a8da8ba325
yur1ysnc <sarkisyan16u@gmail.com>
2022-12-17 01:10:10 +0300

    edit class.cpp

--- class.cpp
+++ class.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <time.h>
+#include <string>
+#include <random>
 class University {
     protected:
-    std::string ticket;
     public:
     virtual std::string generator(std::string,int,int,int) = 0;
 };
@@ -9,8 +11,9 @@ class EMGETUU: public University
 {
     int sum = 0;
     int div = 10;
+    std::string ticket = "";
 public:
-    generator(std::string sex, int year,int month, int day) override
+    std::string generator(std::string sex, int year,int month, int day) override
     {
         if (sex == "man")
         {
@@ -25,18 +28,18 @@ public:
         int date = 10000*year+100*month+ day;
         ticket+=std::to_string(date);
         for (int i = 1; i < ticket.size();i++) sum+=(ticket[i] - '0') * (i+1);
-        std:: mt19937 seq(date, time(0));
+        std:: mt19937 seq(date + time(0));
         std:: uniform_int_distribution<> gap(1000,9999);
         std::string rand_number = std::to_string(gap(seq));
-        for (int i = 0; i < rand_number.size();i++) sum+=(rand[i] - '0') * (10+i);
+        for (int i = 0; i < rand_number.size();i++) sum+=(rand_number[i] - '0')*(10+i);
         while (sum % 2 == 1)
         {
             sum = sum - rand_number[2]*(10+2);
-            new_d = ((rand_number[2] - '0')+1)%10 + '0';
-            rand_number = {rand_number[0],rand_number[1],new_d,rand_number[3]};
+            int new_d = ((rand_number[2] - '0')+1)%10 + '0';
+            rand_number = {rand_number[0],rand_number[1],(char)new_d,rand_number[3]};
             sum = sum + new_d*(10+2);
         }
-        ticket+=std::to_string(rand_number);
+        ticket+=rand_number;
         int ost = sum / div;
         for (int i = 0; i < 10;i++)
         {
@@ -52,36 +55,37 @@ public:
 };
 class MIEM : public University
 {
-   int sum = 0;
-   int div = 11;
+    int sum = 0;
+    int div = 11;
+    std::string ticket = "";
 public:
-    generator(std::string sex,int year, int month, int day) override
+    std::string generator(std::string sex,int year, int month, int day) override
     {
         if (sex == "man")
         {
-            ticket+=to_string(8);
+            ticket+=std::to_string(8);
             sum+=8;
         }
         if (sex == "woman")
         {
-            ticket==to_string(4);
+            ticket==std::to_string(4);
             sum+=4;
         }
         int date = 10000*year + 100*month + day;
         ticket+=std::to_string(date);
         for (int i = 1; i < ticket.size();i++) sum+=(ticket[i] - '0') * (i+1);
-        std:: mt19937 seq(date, time(0));
+        std:: mt19937 seq(date + time(0));
         std:: uniform_int_distribution<> gap(10000,99999);
         std::string rand_number = std::to_string(gap(seq));
         for (int i = 0; i < rand_number.size();i++) sum+=(rand_number[i]-'0')*(10+i);
         while (sum % div == 4)
         {
             sum = sum - rand_number[2]*(10+2);
-            new_d = ((rand_number[2] - '0')+1)%10 + '0';
-            rand_number = {rand_number[0],rand_number[1],new_d,rand_number[3],rand_number[4]};
+            int new_d = ((rand_number[2] - '0')+1)%10 + '0';
+            rand_number = {rand_number[0],rand_number[1],(char)new_d,rand_number[3],rand_number[4]};
             sum = sum + new_d*(10+2);
         }
-        ticket+=std::to_string(rand_number);
+        ticket+=rand_number;
         int ost = sum / div;
         for (int i = 0; i < 10;i++)
         {
@@ -95,3 +99,7 @@ public:
         return ticket;
     }
 };
+int main()
+{
+ return 0;
+}
