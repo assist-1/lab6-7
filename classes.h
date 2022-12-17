3f8e202f0110d59c41d94a8356458974ce618f23
Mukhamed850 <muhamed-11.04.2004@mail.ru>
2022-12-17 14:45:29 +0300

    add class MIEM

--- classes.h
+++ classes.h
@@ -13,6 +13,16 @@ public:
 	virtual std::string pass_generate() = 0;
 };
 
+class PASS_MIEM : public PassTiket {
+public:
+	PASS_MIEM(int sex, int year, int month, int day) {
+		this->sex   = (sex == 1) ? 8 : 4;
+		this->year  = year;
+		this->month = month;
+		this->day   = day;
+	}
+	std::string pass_generate() override;
+}
 
 
 #endif
