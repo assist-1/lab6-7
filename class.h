16f366405039b34fb826066df7522f02d1bdb1f1
yur1ysnc <sarkisyan16u@gmail.com>
2022-12-18 10:57:33 +0300

    edit name func

---
+++ class.h
@@ -0,0 +1,10 @@
+#ifndef CLASSES
+#define CLASSES
+void FromMenu();
+void ToMenu();
+void FromToMenu();
+void Menu();
+void FromFile(char* namefile);
+void FromToFile(char* in_file, char* to_file);
+void ToFile(char* namefile);
+#endif
