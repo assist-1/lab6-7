c3501e64183408c92b5434ec9c8a6975b48ead28
Sorochaikin Alexander <113535520+AlexSiplyy@users.noreply.github.com>
2022-12-20 12:08:10 +0300

    Add files via upload

---
+++ classes.hpp
@@ -0,0 +1,142 @@
+#ifndef UNTITLED_CLASS_HPP
+#define UNTITLED_CLASS_HPP
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
+
+class Student_Ticket_Generator {
+public:
+    int Summurise(const std::string &str)  {
+        int sum = 0;
+        for (size_t i = 0; i < str.size(); ++i) {
+            char c = str[i];
+            sum += ((c - '0') * (i+1));
+        }
+        return sum;
+    }
+
+    int Random(int min, int max) {
+        return min + std::rand() % (max - min);
+    }
+
+    virtual int
+    Generate_random_number(std::string &year, std::string &month, std::string &day,
+                           std::map <std::string, std::vector<int> > &randomNumbers) = 0;
+
+    virtual std::string
+    generate(std::string &sex, std::string &year, std::string &month, std::string &day,
+             std::map <std::string, std::vector<int> > &randomNumbers) = 0;
+};
+
+class MGTU : public virtual Student_Ticket_Generator {
+public:
+    int
+    Generate_random_number(std::string &year, std::string &month, std::string &day,
+                           std::map <std::string, std::vector<int> > &randomNumbers) override {
+        std::string s = (year) + (month) + (day);
+        int number = Random(1000, 10000);
+        if (randomNumbers.find(s) != randomNumbers.end()) {
+            for (size_t i = 0; i < randomNumbers.find(s)->second.size(); ++i) {
+                if (randomNumbers[s][i] == number) {
+                    number = Random(1000, 10000);
+                } else {
+                    randomNumbers[s].push_back(number);
+                }
+            }
+        } else {
+            randomNumbers[s] = {};
+            randomNumbers[s].push_back(number);
+        }
+        return number;
+    }
+
+    std::string
+    generate(std::string &sex, std::string &year, std::string &month, std::string &day,
+             std::map <std::string, std::vector<int> > &randomNumbers) override {
+        std::string gender;
+        if (sex == "man") {
+            gender = "2";
+        } else if (sex == "woman") {
+            gender = "1";
+        }
+        int number = Generate_random_number(year, month, day, randomNumbers);
+        std::string s =
+                gender + (year) + (month) + (day) + std::to_string(number);
+        int Sum = Summurise(s);
+        for (int i = 0; i < 10; ++i) {
+            if ((Sum + i) % 11 == 0) {
+                s = s + std::to_string(i);
+                break;
+            }
+        }
+        return s;
+    }
+};
+
+class MIEM : public virtual Student_Ticket_Generator {
+public:
+    int
+    Generate_random_number(std::string &year, std::string &month, std::string &day,
+                           std::map <std::string, std::vector<int> > &randomNumbers) override {
+        std::string s = (year) + (month) + (day);
+        int number = Random(10000, 100000);
+        if (randomNumbers.find(s) != randomNumbers.end()) {
+            for (size_t i = 0; i < randomNumbers.find(s)->second.size(); ++i) {
+                if (randomNumbers[s][i] == number) {
+                    number = Random(10000, 100000);
+                } else {
+                    randomNumbers[s].push_back(number);
+                }
+            }
+        } else {
+            randomNumbers[s] = {};
+            randomNumbers[s].push_back(number);
+        }
+        return number;
+    }
+
+    std::string
+    generate(std::string &sex, std::string &year, std::string &month, std::string &day,
+             std::map <std::string, std::vector<int> > &randomNumbers) override {
+        std::string gender;
+        if (sex == "man") {
+            gender = "8";
+        } else if (sex == "woman") {
+            gender = "4";
+        }
+        int number = Generate_random_number(year, month, day, randomNumbers);
+        std::string s =
+                gender + (year) + (month) + (day) + std::to_string(number);
+        int Sum = Summurise(s);
+        for (int i = 0; i < 10; ++i) {
+            if ((Sum + i) % 11 == 0) {
+                s = s + std::to_string(i);
+                break;
+            }
+        }
+        return s;
+    }
+};
+
+struct Generator {
+    Student_Ticket_Generator* generator(std::string university) {
+        if (university == "MGTU") {
+            MGTU *a = new MGTU;
+            return a;
+        } else if (university == "MIEM") {
+            MIEM *a = new MIEM;
+            return a;
+        }
+        return nullptr;
+    }
+};
+void CorrectInformation(std::string &year, std::string &month, std::string &day);
+
+bool
+CheckInformation(std::string &university, std::string &sex, std::string &year, std::string &month, std::string &day);
+
+#endif //UNTITLED_CLASS_HPP
